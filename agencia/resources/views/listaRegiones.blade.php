1cc49f223946d66cdf531b397247a2be7cd64c17
marcos pinardi <exegeses@gmail.com>
2021-01-13 21:19:49 -0300

    backup base agencia

---
+++ agencia/resources/views/listaRegiones.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <h1>Listado de regiones</h1>
+
+
+    </main>
+</body>
+</html>
