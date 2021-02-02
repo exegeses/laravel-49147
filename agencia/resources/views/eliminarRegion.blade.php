8b7d6882504a82aaaf3913d208645a003568cf40
Marcos Pinardi <marcos@iMac-de-Marcos.local>
2021-02-01 21:16:02 -0300

    confirmación de baja de una región integrando sweet alert 2

--- agencia/resources/views/eliminarRegion.blade.php
+++ agencia/resources/views/eliminarRegion.blade.php
@@ -6,9 +6,27 @@
         <div class="bg-light col-6 mx-auto shadow rounded p-4 text-danger">
             Se eliminará la siguiente región
             <span class="lead">{{ $region->regNombre }}</span>
-
-
+            <form action="/eliminarRegion" method="post">
+            @csrf
+                <input type="hidden" name="regID"
+                       value="{{ $region->regID }}">
+                <input type="hidden" name="regNombre"
+                       value="{{ $region->regNombre }}">
+                <button class="btn btn-danger btn-block my-3">
+                    Confirmar baja
+                </button>
+                <a href="/adminRegiones" class="btn btn-warning btn-block">
+                    Volver a panel
+                </a>
+            </form>
 
         </div>
+        <script>
+            Swal.fire(
+                'Advertencia',
+                'Si pulsa "Confirma baja", se eliminará la región.',
+                'warning'
+            );
+        </script>
 
     @endsection
