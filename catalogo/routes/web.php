60ce5e6a7540ac4c7fe07f43bcaabdc79d8ca004
Marcos Pinardi <exegeses@gmail.com>
2021-02-17 20:35:37 -0300

    ruta para modificar una marca

--- catalogo/routes/web.php
+++ catalogo/routes/web.php
@@ -24,5 +24,5 @@ Route::get('/adminMarcas', [ MarcaController::class, 'index'  ] );
 Route::get('/agregarMarca', [ MarcaController::class, 'create' ] );
 Route::post('/agregarMarca', [ MarcaController::class, 'store' ]);
 Route::get('/modificarMarca/{id}', [ MarcaController::class, 'edit' ] );
-
+Route::put('/modificarMarca', [ MarcaController::class, 'update' ]);
 
