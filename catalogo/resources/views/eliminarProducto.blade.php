74cdc3bde65bea32bd85e20120c34c5524a93805
Marcos Pinardi <exegeses@gmail.com>
2021-03-01 20:32:34 -0300

    formulario de confirmación de baja de un producto

--- catalogo/resources/views/eliminarProducto.blade.php
+++ catalogo/resources/views/eliminarProducto.blade.php
@@ -6,19 +6,23 @@
 
         <div class="row alert bg-light border-danger col-8 mx-auto p-2">
             <div class="col">
-                <img src="/productos/noDisponible.jpg" class="img-thumbnail">
+                <img src="/productos/{{ $Producto->prdImagen }}" class="img-thumbnail">
             </div>
             <div class="col text-danger align-self-center">
             <form action="/eliminarProducto" method="post">
+            @csrf
+            @method('delete')
 
-                <h2>Nombre del producto</h2>
-                Categoría: catNombre  <br>
-                Marca:  mkNombre <br>
-                Presentación: prdPresentacion <br>
-                Precio: $ prdPrecio
+                <h2>{{ $Producto->prdNombre }}</h2>
+                Categoría: {{ $Producto->relCategoria->catNombre  }}  <br>
+                Marca:  {{ $Producto->relMarca->mkNombre }} <br>
+                Presentación: {{ $Producto->prdPresentacion }} <br>
+                Precio: ${{ $Producto->prdPrecio }}
 
                 <input type="hidden" name="idProducto"
-                       value="idProducto">
+                       value="{{ $Producto->idProducto }}">
+                <input type="hidden" name="prdNombre"
+                       value="{{ $Producto->prdNombre }}">
                 <button class="btn btn-danger btn-block my-3">Confirmar baja</button>
                 <a href="/adminProductos" class="btn btn-outline-secondary btn-block">
                     Volver a panel
