be438eb4a20770f935a1d2df4c0b721f8d4511fa
Marcos Pinardi <exegeses@gmail.com>
2021-03-03 20:03:25 -0300

    notas de instalación de jetstream + livewire

---
+++ login/resources/views/productos.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12 col-8 m-auto">
+        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+             hola
+        </div>
+    </div>
+</x-app-layout>
